dc503902790c6ffc8682a14319732b09a06c9f88
Gage Larsen <gagelarsen53@gmail.com>
2019-02-19 12:12:34 -0700

    speed up dashboard and searching

--- test_tracker/models/test_case.py
+++ test_tracker/models/test_case.py
@@ -109,7 +109,7 @@ class TestCase(models.Model):
                 with open(_json_file, 'r') as jf:
                     data = json.load(jf)
             else:
-                data = json.load(_json_file)
+                date = json.load()
         except IOError:
             errors.append("Could not read file: {}".format(_json_file))
         except ValueError:
