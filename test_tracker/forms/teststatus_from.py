bc130e4126883cabb3db7699f5589bcacf14d9ad
Gage Larsen <gagelarsen53@gmail.com>
2019-02-19 14:39:58 -0700

    add color picker for status colors

--- test_tracker/forms/teststatus_from.py
+++ test_tracker/forms/teststatus_from.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.widgets import TextInput
 from test_tracker.models.test_status import TestStatus
 from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
 
@@ -7,3 +8,7 @@ class TestStatusForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
     class Meta:
         model = TestStatus
         fields = ['status', 'hex_color', 'text_hex_color']
+        widgets = {
+            'hex_color': TextInput(attrs={'type': 'color'}),
+            'text_hex_color': TextInput(attrs={'type': 'color'}),
+        }
