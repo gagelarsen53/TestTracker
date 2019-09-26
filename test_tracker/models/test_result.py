09b0db4e0d20a5837ab51bab6be6964b803cd962
gagelarsen <glarsen@aquaveo.com>
2019-09-26 15:51:06 -0600

    fix merge issue

--- test_tracker/models/test_result.py
+++ test_tracker/models/test_result.py
@@ -82,6 +82,7 @@ class TestResult(models.Model):
                 continue
 
             test_name_re = r'Script Test Log \[[A-Za-z_]*\\(?:[Tt]est_)?([A-Za-z0-9_]*)\]'
+            test_name_re_old = r'Script Test Log \[[A-Za-z_]*\\(?:[Tt]est_)?([A-Za-z0-9_]*)\]'
             test_name_search = re.search(test_name_re, name)
             if not test_name_search:
                 test_name_search = re.search(test_name_re_old, name)
