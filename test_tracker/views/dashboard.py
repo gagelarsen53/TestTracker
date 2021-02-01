3a974949e0ff7b07d42ef63f39df9c098d43b04c
gagelarsen <gagelarsen53@gmail.com>
2021-02-01 07:54:29 -0700

    update

--- test_tracker/views/dashboard.py
+++ test_tracker/views/dashboard.py
@@ -12,7 +12,8 @@ import datetime
 import matplotlib.pyplot as plt
 
 from django.contrib.auth.decorators import login_required
-from django.db import transaction
+from django.db import models, transaction
+from django.forms.models import model_to_dict
 from django.http import HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
@@ -24,5 +25,36 @@ from test_tracker.models.test_result import TestResult
 
 
 def dashboard(request, name, version):
-    context = {}
+    product = Product.objects.get(name=name, version=version)
+    request_starting_date = request.GET.get('starting_date', None)
+    requested_days = request.GET.get('requested_days', 5)
+
+    try:
+        if request_starting_date is not None:
+            starting_date = models.DateField().to_python(request_starting_date)
+        else:
+            starting_date = datetime.date.today()
+    except Exception:
+        starting_date = datetime.date.today()
+
+    table = [
+        {
+            'name': case.name,
+            'category': case.category,
+            'subcategory': case.subcategory,
+            'results': TestResult.objects.filter(
+                testcase=case,
+                date__range=[starting_date - datetime.timedelta(days=int(requested_days)), starting_date]
+            ),
+        }
+        for case in TestCase.objects.filter(product=product)
+    ]
+
+    context = {
+        'product': product,
+        'starting_date': starting_date,
+        'requested_days': requested_days,
+        'table': table,
+    }
+
     return render(request, "test_tracker/dashboard.html", context)
