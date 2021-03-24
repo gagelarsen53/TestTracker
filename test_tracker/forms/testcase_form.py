08ba773a13f4baa7fa714cbf33abe796a292bb9c
mlebaron <mlebaron@aquaveo.com>
2021-03-24 14:18:46 -0600

    Fixed the naming of Product, Category and Subcategory in the create.html.

--- test_tracker/forms/testcase_form.py
+++ test_tracker/forms/testcase_form.py
@@ -1,5 +1,8 @@
 from django import forms
 from test_tracker.models.test_case import TestCase
+from test_tracker.models.product import Product
+from test_tracker.models.test_category import TestCategory
+from test_tracker.models.test_subcategory import TestSubcategory
 from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
 
 
@@ -8,3 +11,18 @@ class TestCaseForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
         model = TestCase
         fields = ['product', 'name', 'source', 'summary', 'category', 'subcategory',
                   'active', 'needs_review', 'create_date', 'author']
+
+    def __init__(self, *args, **kwargs):
+        super(TestCaseForm, self).__init__(*args, **kwargs)
+
+        all_products = Product.objects.all()
+        products = [(product.id, f'{product.name}-{product.version}') for product in all_products]
+        self.fields['product'].choices = products
+
+        all_categories = TestCategory.objects.all()
+        categories = [(category.id, category.category) for category in all_categories]
+        self.fields['category'].choices = categories
+
+        all_subcategories = TestSubcategory.objects.all()
+        subcategories = [(subcategory.id, subcategory.subcategory) for subcategory in all_subcategories]
+        self.fields['subcategory'].choices = subcategories
