01ff88840a034032eb7e1c61713ddcf56cbad76a
mlebaron <mlebaron@aquaveo.com>
2021-03-24 15:53:58 -0600

    Fixed the naming of Authors in the create.html.

--- test_tracker/forms/testcase_form.py
+++ test_tracker/forms/testcase_form.py
@@ -1,8 +1,11 @@
 from django import forms
-from test_tracker.models.test_case import TestCase
+from django.contrib.auth.models import User
+
 from test_tracker.models.product import Product
+from test_tracker.models.test_case import TestCase
 from test_tracker.models.test_category import TestCategory
 from test_tracker.models.test_subcategory import TestSubcategory
+
 from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
 
 
@@ -15,14 +18,18 @@ class TestCaseForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(TestCaseForm, self).__init__(*args, **kwargs)
 
-        all_products = Product.objects.all()
+        all_products = Product.objects.all().order_by('name')
         products = [(product.id, f'{product.name}-{product.version}') for product in all_products]
         self.fields['product'].choices = products
 
-        all_categories = TestCategory.objects.all()
+        all_categories = TestCategory.objects.all().order_by('category')
         categories = [(category.id, category.category) for category in all_categories]
         self.fields['category'].choices = categories
 
-        all_subcategories = TestSubcategory.objects.all()
+        all_subcategories = TestSubcategory.objects.all().order_by('subcategory')
         subcategories = [(subcategory.id, subcategory.subcategory) for subcategory in all_subcategories]
         self.fields['subcategory'].choices = subcategories
+
+        users = User.objects.all().order_by('username')
+        authors = [(user.id, user.username) for user in users]
+        self.fields['author'].choices = authors
